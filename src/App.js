f441d686aede2710d1d065c23ba8b7b17da3cdb3
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-01-09 19:41:38 +0500

    added source code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import Home from "./pages/home/Home.page";
 
-function App() {
-  console.log(process.env);
+const App = () => {
   return (
     <div>
       <Home />
     </div>
   );
-}
+};
 
 export default App;
